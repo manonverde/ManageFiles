6015e935b61f00ebcd83214cc94f7ac08bb027e2
Manon Verdenal <manon@MacBook-Air-de-Manon.local>
2018-02-21 14:19:22 +0100

    controller for doc

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,19 @@
+class DocsController < ApplicationController
+  def show
+    # ...
+  end
+  def edit
+    # ...
+  end
+  def update
+    # ...
+  end
+  def destroy
+    # ...
+  end
+
+  private
+
+  def find_doc
+  end
+end
