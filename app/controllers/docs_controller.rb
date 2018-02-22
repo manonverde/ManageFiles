e5761e7cc4370d28ae311b96de60e3e2e4054088
Manon Verdenal <manon@MacBook-Air-de-Manon.local>
2018-02-22 11:03:00 +0100

    before style

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,19 +1,47 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @docs = Doc.where(user_id: current_user)
+  end
+
   def show
-    # ...
   end
-  def edit
-    # ...
+
+  def new
+    @doc = current_user.docs.build
   end
+
   def update
-    # ...
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
-  def destroy
-    # ...
+
+    def destroy
+      @doc.destroy
+      redirect_to docs_path
+    end
+
+
+  def create
+    @doc = current_user.docs.build(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
 
   private
 
   def find_doc
+    @doc = Doc.find(params[:id])
+  end
+
+  def doc_params
+    params.require(:doc).permit(:title, :content)
   end
 end
